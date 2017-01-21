343fd0e1e165feaebd712954a8a83369dece45be
cristina-necula <cristina.necula20@gmail.com>
2017-01-21 23:05:49 +0200

    Added shell page with buttons for register and authenticate:

---
+++ AndroidBiometricAuthentication/app/src/main/java/com/biometric/android/androidbiometricauthentication/activities/ShellActivity.java
@@ -0,0 +1,41 @@
+package com.biometric.android.androidbiometricauthentication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+
+import com.biometric.android.androidbiometricauthentication.R;
+
+public class ShellActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shell);
+
+        initUIElements();
+    }
+
+    private void initUIElements(){
+        final Button registerButton = (Button) findViewById(R.id.button_register);
+        registerButton.setOnClickListener(this);
+
+        final Button authenticateButton = (Button) findViewById(R.id.button_authenticate);
+        authenticateButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_authenticate:
+                // TODO - navigate to authenticate page
+                break;
+            case R.id.button_register:
+                // TODO - navigate to register page
+                break;
+        }
+    }
+}
