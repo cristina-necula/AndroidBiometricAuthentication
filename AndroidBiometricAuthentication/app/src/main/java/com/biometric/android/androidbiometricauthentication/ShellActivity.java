8964d80386180021f8c7297122ff723cdb359305
cristina-necula <cristina.necula20@gmail.com>
2017-01-21 22:49:18 +0200

    Added android project

---
+++ AndroidBiometricAuthentication/app/src/main/java/com/biometric/android/androidbiometricauthentication/ShellActivity.java
@@ -0,0 +1,13 @@
+package com.biometric.android.androidbiometricauthentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShellActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shell);
+    }
+}
